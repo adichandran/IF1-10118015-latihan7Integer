2fd784827e0ea8c9ccdf22017109523e80411809
adichandran <55467774+adichandran@users.noreply.github.com>
2019-09-24 10:22:59 +0700

    Add files via upload

---
+++ src/if1/pkg101108015/latihan7integer/IF1101108015Latihan7integer.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg101108015.latihan7integer;
+
+/**
+ *
+ * @author 
+ * NAMA                 : Adi Chandra Nugraha
+ * KELAS                : IF-1
+ * NIM                  : 10118015
+ * Deskripsi Program    : Program Ini Berisi Program dengan variabel integer
+ */
+public class IF1101108015Latihan7integer {
+
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 900000000000L;
+        System.out.println("byte : "+ b);
+        System.out.println("short integer : " + s);
+        System.out.println("integer : " + i);
+        System.out.println("long integer : " + l);
+    }
+    
+}
